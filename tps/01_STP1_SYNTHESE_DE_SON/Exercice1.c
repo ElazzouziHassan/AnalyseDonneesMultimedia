6f85c70271d52c851cbe23b6f839abb8f0009338
wizardy <wizardhassan99@gmail.com>
2024-11-15 13:20:42 +0100

    commit

--- tps/01_STP1_SYNTHESE_DE_SON/Exercice1.c
+++ tps/01_STP1_SYNTHESE_DE_SON/Exercice1.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 
-#define PI 3.14159265358979323846
+#define F 440
+#define A 20000
+#define FE 44100
+#define DUREE 1 
 
 int main() {
-    int F = 440;           
-    int A = 20000;     
-    int Fe = 44100;       
-    int D = 1;            
+    int n = FE * DUREE;  
+    int16_t T[n];   
 
-    int N = Fe * D;
-
-    int16_t *T = (int16_t*)malloc(N * sizeof(int16_t));
-
-    if (T == NULL) {
-        printf("Erreur d'allocation mémoire\n");
-        return 1;
+    for (int i = 0; i < n; i++) {
+        double t = (double)i / FE;
+        T[i] = (int16_t)(A * sin(2 * M_PI * F * t));
     }
 
-    // Génération du signal sinusoïdal
-    for (int n = 0; n < N; n++) {
-        T[n] = (int16_t)(A * sin(2 * PI * F * n / Fe));
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", T[i]);
     }
-
-    // Libération de la mémoire
-    free(T);
-
-    printf("Signal généré avec succès\n");
 
     return 0;
 }
+
