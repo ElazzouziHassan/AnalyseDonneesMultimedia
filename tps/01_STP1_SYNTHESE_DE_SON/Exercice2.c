d8265dff67c3bdc739907e21d612bf9f765f27c8
wizardy <wizardhassan99@gmail.com>
2024-11-22 15:35:48 +0100

    commit

--- tps/01_STP1_SYNTHESE_DE_SON/Exercice2.c
+++ tps/01_STP1_SYNTHESE_DE_SON/Exercice2.c
@@ -10,10 +10,18 @@
 int main() {
     int n = FE * DUREE;
     int16_t T[n];
+    int16_t TDOUBLE[n];
+    int16_t TTRIPLE[n];
+
+    int16_t TFINAL[n];
 
     for (int i = 0; i < n; i++) {
         double t = (double)i / FE;
         T[i] = (int16_t)(A * sin(2 * M_PI * F * t));
+        TDOUBLE[i] = (int16_t)(A * sin(2 * M_PI * F * 2 * t));
+        TTRIPLE[i] = (int16_t)(A * sin(2 * M_PI * F * 3 * t));
+
+        TFINAL[i] = T[i] + TDOUBLE[i] + TTRIPLE[i];
     }
 
     FILE *f = fopen("Sinus.txt", "w");
@@ -23,7 +31,8 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        fprintf(f, "%d\n", T[i]);
+        // fprintf(f, "%d\n", T[i]);
+        fprintf(f, "%d\n", TFINAL[i]);
     }
 
     fclose(f);
